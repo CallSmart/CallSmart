99ae63f4038500c314cc11c3315217598ca282b3
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-02-07 00:53:42 -0500

    small style change

--- app/account/updatePassword/page.tsx
+++ app/account/updatePassword/page.tsx
@@ -1,39 +1,46 @@
-'use client'
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import ProductNavBar from "@/components/ProductNavBar";
 import { supabase } from "../../../supabase"; // Uncomment if supabase is used
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from "next/dist/server/api-utils";
+import { Card } from "@tremor/react";
 
 function Page() {
-    const [pswd, setPswd] = useState("");
+  const [pswd, setPswd] = useState("");
 
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPswd(event.target.value);
-    }
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPswd(event.target.value);
+  };
 
-    const handlePasswordSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(pswd); // For demonstration, replace with your function
-        // Add your logic to handle password submission
-        await supabase.auth.updateUser({ password: pswd })
-        alert('You may now log in')
-    }
+  const handlePasswordSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    console.log(pswd); // For demonstration, replace with your function
+    // Add your logic to handle password submission
+    await supabase.auth.updateUser({ password: pswd });
+    alert("You may now log in");
+  };
 
-    return (
-        <ProductNavBar>
-            <div>reset password</div>
-            <form onSubmit={handlePasswordSubmit}>
-                <input 
-                    type="password" 
-                    name="password" 
-                    placeholder="Enter Password" 
-                    value={pswd} 
-                    onChange={handlePasswordChange} 
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </ProductNavBar>
-    );
+  return (
+    <ProductNavBar>
+      <Card className="flex flex-col w-1/2 border-prim-blue" decoration="top">
+        <p className="font-semibold">Update Password</p>
+        <form className="flex flex-col gap-4" onSubmit={handlePasswordSubmit}>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter New Password"
+            value={pswd}
+            onChange={handlePasswordChange}
+          />
+          <button className="btn-submit" type="submit">
+            Submit
+          </button>
+        </form>
+      </Card>
+    </ProductNavBar>
+  );
 }
 
 export default Page;
