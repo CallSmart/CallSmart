9a96365e3d4831ab7303e4f8b189834fcae3f72c
gonzaloandresoto <gonzandresoto@gmail.com>
2024-02-18 23:54:06 -0500

    fixed navbar on auth routes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,13 +1,14 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 // import { Auth } from '@supabase/ui';
-import { supabase } from "../../supabase";
-import { redirect } from "next/navigation";
-import Sidebar from "@/components/sidebar";
-import HomeNavBar from "@/components/HomeNavBar";
-import { Card } from "@tremor/react";
+import { supabase } from '../../supabase';
+import { redirect } from 'next/navigation';
+import Sidebar from '@/components/sidebar';
+import HomeNavBar from '@/components/HomeNavBar';
+import { Card } from '@tremor/react';
+import NavBar from '@/components/landing/main/NavBar';
 
 const LoginPage = () => {
   const router = useRouter();
@@ -19,35 +20,35 @@ const LoginPage = () => {
       const user = await supabase.auth.getUser();
       const session = await supabase.auth.getSession();
       if (user.data.user?.id != null) {
-        console.log("User is logged in");
+        console.log('User is logged in');
         localStorage.setItem(
-          "token",
+          'token',
           session.data.session?.access_token as string
         );
-        localStorage.setItem("user", user.data.user?.id as string);
-        console.log("User is logged in");
-        router.push("/dashboard");
+        localStorage.setItem('user', user.data.user?.id as string);
+        console.log('User is logged in');
+        router.push('/dashboard');
       }
     }
     checkUser();
   }, [router]);
 
   const handleForgotPassword = async (email: string) => {
-    console.log("EMAIL PASSED INTO FORGOT PASSWORD" + email);
+    console.log('EMAIL PASSED INTO FORGOT PASSWORD' + email);
 
     try {
       const { error } = await supabase.auth.resetPasswordForEmail(email);
 
       if (error) {
-        console.error("Error sending password recovery email:", error.message);
+        console.error('Error sending password recovery email:', error.message);
       } else {
-        console.log("Password recovery email sent successfully!");
+        console.log('Password recovery email sent successfully!');
         window.alert(
-          "Password recovery email sent successfully! Make sure to check your spam folder."
+          'Password recovery email sent successfully! Make sure to check your spam folder.'
         );
       }
     } catch (error) {
-      console.error("Error sending password recovery email:", error);
+      console.error('Error sending password recovery email:', error);
     }
   };
 
@@ -64,20 +65,21 @@ const LoginPage = () => {
     });
     if (error) {
       setSignInError(true);
-      console.log("Login error:", error.message);
+      console.log('Login error:', error.message);
     } else {
-      console.log("Login success:", data);
-      router.push("/dashboard");
+      console.log('Login success:', data);
+      router.push('/dashboard');
     }
   };
 
   return (
-    <HomeNavBar>
-      <h1 className="text-center w-1/3">Welcome Back!</h1>
+    <div className='flex flex-col items-center gap-24 py-12 md:pt-16 lg:pt-24'>
+      <NavBar />
+      <h1 className='text-center w-1/3'>Welcome Back!</h1>
       {forgotPassword ? (
         <Card
-          decoration="top"
-          className="w-1/2 min-w-[400px] flex flex-col p-4 gap-2 z-50 static text-sec-blue"
+          decoration='top'
+          className='w-1/2 min-w-[400px] flex flex-col p-4 gap-2 z-50 static text-sec-blue'
         >
           <form
             onSubmit={(e) => {
@@ -87,32 +89,35 @@ const LoginPage = () => {
               };
               handleForgotPassword(target.email.value);
             }}
-            className="flex flex-col gap-2 indent-4"
+            className='flex flex-col gap-2 indent-4'
           >
-            <h3 className="text-center text-4xl font-semibold indent-0">
+            <h3 className='text-center text-4xl font-semibold indent-0'>
               Submit
             </h3>
-            <hr className="my-2" />
-            <div className="form-section">
-              <label className="flex gap-1">Email</label>
+            <hr className='my-2' />
+            <div className='form-section'>
+              <label className='flex gap-1'>Email</label>
               <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="callsmart@support.ca"
+                type='email'
+                id='email'
+                name='email'
+                placeholder='callsmart@support.ca'
                 required
               />
             </div>
 
-            <hr className="my-2" />
-            <div className="form-section">
+            <hr className='my-2' />
+            <div className='form-section'>
               <button
                 onClick={() => setForgotPassword(false)}
-                className="hover:opacity-50"
+                className='hover:opacity-50'
               >
                 <em>Remembered your password?</em>
               </button>
-              <button type="submit" className="btn-submit">
+              <button
+                type='submit'
+                className='btn-submit'
+              >
                 submit
               </button>
             </div>
@@ -120,11 +125,11 @@ const LoginPage = () => {
         </Card>
       ) : (
         <Card
-          decoration="top"
-          className="w-1/2 min-w-[300px] flex flex-col p-4 gap-2 z-50 static text-sec-blue"
+          decoration='top'
+          className='w-1/2 min-w-[300px] flex flex-col p-4 gap-2 z-50 static text-sec-blue'
         >
           <form
-            id="loginForm"
+            id='loginForm'
             onSubmit={(e) => {
               e.preventDefault();
               const target = e.target as typeof e.target & {
@@ -136,61 +141,67 @@ const LoginPage = () => {
                 password: target.password.value,
               });
             }}
-            className="flex flex-col gap-2 indent-4"
+            className='flex flex-col gap-2 indent-4'
           >
-            <h3 className="text-center text-4xl font-semibold indent-0">
+            <h3 className='text-center text-4xl font-semibold indent-0'>
               Log In
             </h3>
-            <hr className="my-2" />
-            <div className="form-section">
-              <label className="flex gap-1">Email</label>
+            <hr className='my-2' />
+            <div className='form-section'>
+              <label className='flex gap-1'>Email</label>
               <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="callsmart@support.ca"
+                type='email'
+                id='email'
+                name='email'
+                placeholder='callsmart@support.ca'
                 required
               />
             </div>
-            <div className="form-section">
-              <label className="flex gap-1">Password</label>
+            <div className='form-section'>
+              <label className='flex gap-1'>Password</label>
               <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="SuperSecure123"
+                type='password'
+                id='password'
+                name='password'
+                placeholder='SuperSecure123'
                 required
               />
             </div>
-            <hr className="my-2" />
-            <div className="form-section">
+            <hr className='my-2' />
+            <div className='form-section'>
               <a
                 onClick={() => setForgotPassword(true)}
-                className="hover:opacity-50"
+                className='hover:opacity-50'
               >
                 <em>Forgot username or password?</em>
               </a>
               {signInError ? (
-                <p className="text-red-500">
+                <p className='text-red-500'>
                   Incorrect username or password, or user does not exist
                 </p>
               ) : null}
-              <button type="submit" className="btn-submit">
+              <button
+                type='submit'
+                className='btn-submit'
+              >
                 Sign In
               </button>
             </div>
           </form>
         </Card>
       )}
-      <span className="flex">
+      <span className='flex'>
         <p>
-          <em className="opacity-50">Don't have an account? </em>
-          <a className="hover:opacity-50" href="/signup">
+          <em className='opacity-50'>Don't have an account? </em>
+          <a
+            className='hover:opacity-50'
+            href='/signup'
+          >
             Join us!
           </a>
         </p>
       </span>
-    </HomeNavBar>
+    </div>
   );
 };
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,29 +1,31 @@
-"use client";
-import HomeNavBar from "@/components/HomeNavBar";
-import { Card } from "@tremor/react";
-import { useRouter } from "next/navigation";
-import React from "react";
+'use client';
+import HomeNavBar from '@/components/HomeNavBar';
+import NavBar from '@/components/landing/main/NavBar';
+import { Card } from '@tremor/react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
 
 const ChoosePlan = () => {
   const router = useRouter();
 
   const option = () => {
-    router.push("/signup/contactForm");
+    router.push('/signup/contactForm');
   };
 
   return (
-    <HomeNavBar>
-      <h1 className="text-center">Your Plan</h1>
+    <div className='flex flex-col items-center gap-24 py-12 md:pt-16 lg:pt-24'>
+      <NavBar />
+      <h1 className='text-center'>Your Plan</h1>
       <Card
-        decoration="top"
-        className="w-3/4 border-prim-blue flex flex-col gap-4 items-center"
+        decoration='top'
+        className='w-3/4 border-prim-blue flex flex-col gap-4 items-center'
       >
-        <h4 className="text-prim-blue">Local Clinic</h4>
+        <h4 className='text-prim-blue'>Local Clinic</h4>
         <h3>
           <em>$249.99</em>
         </h3>
-        <p className="font-semibold">Our monthly plan includes:</p>
-        <ul className="list-disc ml-4">
+        <p className='font-semibold'>Our monthly plan includes:</p>
+        <ul className='list-disc ml-4'>
           <li>Missed call text-back automation for dental clinics</li>
           <li>Instant client engagement with automated messages</li>
           <li>
@@ -33,11 +35,14 @@ const ChoosePlan = () => {
           <li>Customizable message templates to suit your clinic's needs</li>
           <li>Customer support for any assistance you require</li>
         </ul>
-        <a className="btn-action" href="/signup/contactForm">
+        <a
+          className='btn-action'
+          href='/signup/contactForm'
+        >
           Select
         </a>
       </Card>
-    </HomeNavBar>
+    </div>
   );
 };
 
